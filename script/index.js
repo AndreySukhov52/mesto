bedd462444b86d800fb40b373164591ba1d70da8
Andrey Sukhov <andrei11152@yandex.ru>
2022-12-07 16:22:03 +0300

    fix: изменили свойства для блоков и справили js

--- script/index.js
+++ script/index.js
@@ -1,37 +1,50 @@
-const popupOpen = document.querySelector('.profile__edit'); // найти кнопку рекадтировать профиль
-const popup = document.querySelector('.popup');             // найти селектор блока popup       
-const popupClose = document.querySelector('.popup__close'); // найти кнопку закрыть в форме.
-const popupButtonSave = document.querySelector('.popup__button'); // найти кнопка сохранить в форме.
+/** найти кнопку рекадтировать профиль */
+const popupOpen = document.querySelector('.profile__edit');
+/** найти селектор блока popup */
+const popup = document.querySelector('.popup');
+/** найти форму с классом .popup__form */
+const popupForm = document.querySelector('.popup__form');
+/** найти кнопку закрыть в форме. */ 
+const popupClose = document.querySelector('.popup__close');
+/** найти элемент с классом .profile__name и записать в переменную profileName */
+let profileName = document.querySelector('.profile__name');
+/** найти элемент с классом .profile__about-me и записать в переменную profileAbout */
+let profileAbout = document.querySelector('.profile__about-me');
+/** найти input с классом .popup__input_item_name и записать в переменную valueName */
+let valueName = document.querySelector('.popup__input_item_name');
+/** найти input с классом .popup__input_item_job и записать в переменную valueJob */
+let valueJob = document.querySelector('.popup__input_item_job');
 
-popupOpen.addEventListener('click', openPopup); // слушаем кнопку редактировать профиль .Profile__edit
-popupClose.addEventListener('click', closePopup); // слушаем кнопку закрыть форму .popup__close
-popupButtonSave.addEventListener('click', saveValuePopup); // слушаем кнопку сохранить .popup__button
 
 
+/** добавляем модификатор popup_opened блоку popup */
 function openPopup() {
-    popup.classList.add('popup__opened'); // добавляем класс элементу popup__opened
+    popup.classList.add('popup_opened');
+    valueName.value = profileName.textContent;
+    valueJob.value = profileAbout.textContent;  
 }
 
+/** удаляем модификатор popup_opened у элемента */
 function closePopup() {
-    popup.classList.remove('popup__opened'); // удаляем класс popup__opened у элемента
+    popup.classList.remove('popup_opened');
 }
 
-popup.addEventListener('click', function(event) {  // сравниваем значение события event.target и event.currentTarget, если равны то выполняем функцию закрытия popup
-    if(event.target === event.currentTarget) {
-        closePopup();
-    }
-});
-
+/** функция сохранения данных */
 function saveValuePopup(evt) {
-    let valueName = document.querySelector('.popup__item-name').value; // найти значение value у элемента с классом .popup__item-name
-    let valueJob = document.querySelector('.popup__item-job').value;   // найти значение value у элемента с классом .popup__item-job
-    document.querySelector('.profile__name').innerHTML = valueName;    //  найти элемент с классом .Profile__name и записать в HTML значения из переменной valueName         
-    document.querySelector('.profile__about-me').innerHTML = valueJob; // найти элемент с классом .Profile__about-me и записать в HTML значения из переменной valueJob
-    evt.preventDefault();  // Эта строчка отменяет стандартную отправку формы.
+    /** Эта строчка отменяет стандартную отправку формы. */
+    evt.preventDefault();
+    /** записать текст из value переменной valueName */ 
+    profileName.textContent = valueName.value;
+    /** записать текст из value переменной valueJob */         
+    profileAbout.textContent = valueJob.value;
+    closePopup();
 };
 
-document.addEventListener('keydown', function (e) { //слушаем нажатие клавиш на клавиатуре
-    if (e.which === 13) {      // если код нажатой клавиши равен 13(клавиша enter)
-            saveValuePopup(); // выполняем функцию сохранения данных
-    }
-});
+
+
+/** слушаем кнопку редактировать профиль .Profile__edit */
+popupOpen.addEventListener('click', openPopup);
+/** слушаем кнопку закрыть форму .popup__close */
+popupClose.addEventListener('click', closePopup);
+/** слушаем отрпавку формы по событию 'submit' */
+popupForm.addEventListener('submit', saveValuePopup);
