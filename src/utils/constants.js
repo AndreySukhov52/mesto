f742e3e39eb65121bd51d4f3af695e5ab6bbfd06
Andrey Sukhov <andrei11152@yandex.ru>
2023-03-04 12:54:07 +0300

    fix: исправили block popup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -58,13 +58,12 @@ export const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-  /** конфиг валидации */
-  export const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-  };
- 
+/** конфиг валидации */
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
